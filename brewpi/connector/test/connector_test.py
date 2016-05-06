fb2b30b4218e74727507d16b2820d4d396300ccf
Matthew McGowan <mat.mcgowan+github@gmail.com>
2016-05-06 08:32:03 +0200

    test app working.

--- brewpi/connector/test/connector_test.py
+++ brewpi/connector/test/connector_test.py
@@ -1,11 +1,26 @@
 import logging
+from time import sleep
 
+from brewpi.connector.controlbox.objects import MixinController
 from brewpi.protocol.factory import all_sniffers
 from controlbox.connector_facade import ControllerDiscoveryFacade
+from controlbox.protocol.controlbox import ControlboxProtocolV1
 from controlbox.protocol.io import determine_line_protocol
 
 logger = logging.getLogger(__name__)
 
+
+def dump_device_info(connector, protocol:ControlboxProtocolV1):
+    if not hasattr(protocol, 'controller'):
+        controller = protocol.controller = MixinController(connector)
+        controller.initialize(True)
+    else:
+        controller = protocol.controller
+    id = controller.system_id().read()
+    time = controller.system_time().read()
+    endpoint = connector.endpoint
+    str_id = ''.join('{:02x}'.format(x) for x in id)
+    logger.info("device at '%s' id '%s' current time %s " % (endpoint, str_id, time))
 
 def monitor():
     """ logs the connected devices. A dummy protocol sniffer is used. """
@@ -16,25 +31,28 @@ def monitor():
     def sniffer(conduit):
         return determine_line_protocol(conduit, all_sniffers)
 
-    def handle_connections(connectors):
-        for c in connectors:
+    def handle_connections(managed_connections):
+        for c in managed_connections:
             try:
-                conduit = c.connector.conduit
-                if conduit and conduit.open:
-                    conduit.input.read()
-                    conduit.output.write(b"[]\n")
-                    conduit.output.flush()
+                connector = c.connector
+                if connector.connected:
+                    dump_device_info(connector, connector.protocol)
+
             except Exception as e:
                 logger.exception(e)
                 pass
 
-    discoveries = (builder.build_serial_discovery(sniffer),
-                   builder.build_tcp_server_discovery(sniffer, "brewpi"))
+    discoveries = ( builder.build_serial_discovery(sniffer),
+                   builder.build_tcp_server_discovery(sniffer, "brewpi"),)
     facade = builder(discoveries)
-    while True:
-        # detect any new
-        facade.update()
-        handle_connections(facade.manager.connections.values())
+    try:
+        while True:
+            # detect any new connections
+            facade.update()
+            handle_connections(facade.manager.connections.values())
+            sleep(1)
+    finally:
+        logger.error("exiting main loop")
 
 if __name__ == '__main__':
     monitor()
